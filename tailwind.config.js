3dd32c9a269a20f570efc791bd46ae5689ffdb9f
mdshajibazher <mdshajibazher@gmail.com>
2023-09-24 19:35:46 +0600

    header portion completed

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    screens: {
+      sm: '576px',
+      md: '768px',
+      lg: '992px',
+      xl: '1200px'
+    },
+    container: {
+      center: true,
+      padding: '1rem'
+    },
+    extend: {
+      fontFamily: {
+        roboto: ['Roboto','sans-serif'],
+        poppins: ['Poppins','sans-serif'],
+      },
+      colors: {
+        primary: '#3498db'
+      }
+    },
+  },
+
+
+  plugins: [],
+}
+
